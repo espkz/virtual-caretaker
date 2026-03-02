f4e520590d6e494f7562456d41be305eee66f0d6
espkz <ellie.paek@gmail.com>
2026-03-02 14:47:04 -0800

    prototype django setup

--- vipdjango/vipson/settings.py
+++ vipdjango/vipson/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "vip.apps.VipConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
